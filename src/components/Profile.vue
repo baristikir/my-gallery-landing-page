8bc25252905c4c524f726699738dadde411cd5ce
Baris Tikir <brstkr@icloud.com>
2020-10-18 14:17:43 +0200

    Initial Setup :package:

---
+++ src/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1>Profile</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
